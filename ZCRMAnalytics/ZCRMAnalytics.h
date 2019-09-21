78caa2162645ea8f9dbc5e31be763be1edab1ea0
Karthik Shiva <IMALEGEND@zohoCorp.com>
2019-09-21 11:52:42 +0530

    Initial Commit

---
+++ ZCRMAnalytics/ZCRMAnalytics.h
@@ -0,0 +1,19 @@
+//
+//  ZCRMAnalytics.h
+//  ZCRMAnalytics
+//
+//  Created by Kalyani shiva on 21/09/19.
+//  Copyright © 2019 Kalyani shiva. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZCRMAnalytics.
+FOUNDATION_EXPORT double ZCRMAnalyticsVersionNumber;
+
+//! Project version string for ZCRMAnalytics.
+FOUNDATION_EXPORT const unsigned char ZCRMAnalyticsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZCRMAnalytics/PublicHeader.h>
+
+
